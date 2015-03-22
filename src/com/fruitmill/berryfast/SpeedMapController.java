01708b203b8e6747300a33eadd1a7856906ced50
rachar <ra.rohan@gmail.com>
2015-03-22 13:01:47 -0700

    more framework changes for speedmapcontroller

--- src/com/fruitmill/berryfast/SpeedMapController.java
+++ src/com/fruitmill/berryfast/SpeedMapController.java
@@ -1,10 +1,13 @@
 package com.fruitmill.berryfast;
 
+import com.fruitmill.berryfast.common.Commands;
+
 public class SpeedMapController extends Actor {
 
     private String TAG = "SpeedLight";
-    private Actor commandcenter= null;
+    private Actor commandCenter = null;
     private boolean pause = true;
+    private boolean stop = false;
 
     @Override
 	protected void dispatch(Object[] message) {
@@ -12,6 +15,7 @@ public class SpeedMapController extends Actor {
 			switch (message[0].toString()) {
 			case "init":
                 pause = true;
+                stop = false;
 				break;
             case "start":
                 pause = false;
@@ -21,13 +25,55 @@ public class SpeedMapController extends Actor {
                 break;
 			case "die":
                 pause = true;
+                stop = true;
             }
         } catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+    private Commands generateCommands(double speedDiff) {
+        return new Commands();
+    }
+
+    private double currentSpeed() {
+        return 0.0;
+    }
+
+    private double requiredSpeed() {
+        return 0.0;
+    }
+
+    private void setUpSpeedMap() {
+        return;
+    }
+
+    private void initThread() {
+        setUpSpeedMap();
+        Thread thread = new Thread() {
+            public void run() {
+                while(!stop) {
+                    if (!pause) {
+                        Actor.send(
+                                commandCenter,
+                                new Object[]{
+                                        "send_command",
+                                        generateCommands(
+                                                currentSpeed() - requiredSpeed()).getJSON().getBytes()});
+                    } else {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            continue;
+                        }
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
+
     public void RegisterCommandCenter(Actor cc) {
-        commandcenter = cc;
+        commandCenter = cc;
     }
 }
